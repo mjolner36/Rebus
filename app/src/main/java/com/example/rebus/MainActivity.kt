ca0c787d3807b2330347cd149d9e54d0438bc626
ruslan <mjoruslan@gmail.com>
2022-06-21 01:18:22 +0300

    add main menu

--- app/src/main/java/com/example/rebus/MainActivity.kt
+++ app/src/main/java/com/example/rebus/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.rebus
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun startGame(view: View){
+        var intent = Intent(this@MainActivity,GameActivity::class.java)
+        startActivity(intent)
+    }
+
 }
